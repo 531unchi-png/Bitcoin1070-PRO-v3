c1e3abad51da5821f3591efca71d35db0a24b05d
531unchi-png <531unchi@gmail.com>
2026-09-22 21:07:12 +0900

    fix: expose sibling debt ledger link

--- sibling-apps.js
+++ sibling-apps.js
@@ -3,7 +3,7 @@ window.BITCOIN1070_SIBLING_APPS = Object.freeze([
   Object.freeze({
     id:'pachinko-shushi-note',
     name:'パチンコ・パチスロ収支帳',
-    description:'パチンコ・パチスロ・競馬の収支を月別・年別・累計で管理',
-    url:'https://531unchi-png.github.io/Pachinko-Shushi-Note/'
+    description:'パチンコ・パチスロ・競馬の収支管理＋借金返済帳',
+    url:'https://531unchi-png.github.io/Pachinko-Shushi-Note/#debt'
   })
 ]);
